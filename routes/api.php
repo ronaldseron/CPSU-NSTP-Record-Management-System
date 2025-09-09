e44f976f688c62dd1c961957ef870c10dd9f1bdb
Ronse11 <seronronalddalida@gmail.com>
2025-09-09 17:09:06 +0800

    refactor: removed unused code for clean routes

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,4 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('/form-registration', [StudentFormRegistrationController::class, 'store']);
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/form-registration', [StudentFormRegistrationController::class, 'store'])->middleware('auth:sanctum');

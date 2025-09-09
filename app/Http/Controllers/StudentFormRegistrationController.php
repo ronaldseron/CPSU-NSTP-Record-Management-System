82ee0ff90faa9e0f1e35dfc158e59e815aa7aec2
Ronse11 <seronronalddalida@gmail.com>
2025-09-09 16:46:37 +0800

    refactor: updated for better practice by separating validation

--- app/Http/Controllers/StudentFormRegistrationController.php
+++ app/Http/Controllers/StudentFormRegistrationController.php
@@ -5,12 +5,30 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreStudentFormRequest;
 use App\Models\StudentFormRegistration;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Request;
 
 class StudentFormRegistrationController extends Controller
 {
+        /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(StoreStudentFormRequest $request): JsonResponse
     {
         try {
@@ -27,14 +45,42 @@ class StudentFormRegistrationController extends Controller
                 'data' => $registration
             ], 201);
         } catch (\Throwable $th) {
-        Log::error('Form registration failed', [
-            'message' => $th->getMessage(),
-            'trace' => $th->getTraceAsString(),
-        ]);
             return response()->json([
                 'message' => 'An error occurred while processing your registration',
                 'error' => config('app.debug') ? $th->getMessage() : 'Server error'
             ], 500);
         }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }

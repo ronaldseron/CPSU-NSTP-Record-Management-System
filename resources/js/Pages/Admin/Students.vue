cb6402a3b09c02de2f8d5ae7b5f73fee1c7fe577
Ronse11 <seronronalddalida@gmail.com>
2025-09-11 14:20:58 +0800

    feat: added deletion function

--- resources/js/Pages/Admin/Students.vue
+++ resources/js/Pages/Admin/Students.vue
@@ -4,11 +4,16 @@ import { Head } from '@inertiajs/vue3';
 import BaseHeader from '@/Components/Admin/BaseHeader.vue';
 import BaseProgramPage from '@/Components/Admin/BaseProgramPage.vue';
 import BasePagination from '@/Components/Admin/BasePagination.vue';
+import { useDeleteStudents } from '@/Composables/useDeleteStudents.js';
 
 defineProps({
     students: Object,
     perPage: Number
 });
+
+function deleteStudents(id) {
+    useDeleteStudents(id);
+}
 </script>
 
 <template>
@@ -21,9 +26,22 @@ defineProps({
             <BaseHeader headerName="STUDENTS" />
         </template>
         <!-- Table -->
-        <BaseProgramPage :students="students" :perPage="perPage"/>
+        <BaseProgramPage 
+            :students="students" 
+            :perPage="perPage"
+            @delete="deleteStudents"
+        />
         <!-- Pagination -->
-        <BasePagination :students="students"/>
+        <BasePagination
+            :current-page="students.current_page"
+            :last-page="students.last_page"
+            :from="students.from"
+            :to="students.to"
+            :total="students.total"
+            :prev-page-url="students.prev_page_url"
+            :next-page-url="students.next_page_url"
+            :path="students.path"
+        />
 
     </AdminLayout>
 </template>
